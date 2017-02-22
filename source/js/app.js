b07d47d5524cb8c3ac6adec679fa68b327c5c8dd
maxmarinich <max.marinich@gmail.com>
2017-02-22 18:54:52 +0300

    setup state

--- source/js/app.js
+++ source/js/app.js
@@ -3,8 +3,13 @@ import Carousel from './structure/Carousel';
 
 
 export const App = () =>  (
-    <div className="carousel-wrapper">
+    <div className="app">
         <h1 className="h1">React Carousel Gallery</h1>
-        <Carousel />
+        <Carousel>
+            <div className="item">1</div>
+            <div className="item">2</div>
+            <div className="item">3</div>
+            <div className="item">4</div>
+        </Carousel>
     </div>
 );
--- source/js/structure/Carousel.js
+++ source/js/structure/Carousel.js
@@ -6,21 +6,65 @@ class Carousel extends React.Component {
         super(props);
         this.state = {
             style: {},
+            responsive: {},
+            duration: 400,
+            translate3d: 0,
+            currentIndex: 0,
             showDots: true,
             showButtons: true,
-            children: this.props.children || []
+            slides: this.props.children || []
         };
+        this._resizeHandler = this._resizeHandler.bind(this);
+        this._getGalleryStageItem = this._getGalleryStageItem.bind(this);
     }
 
-    _renderDotsNavigation() {
-        return(
-            <ul className="carousel-dots">
-                <li className="carousel-dots__item __active" />
-                <li className="carousel-dots__item" />
-                <li className="carousel-dots__item" />
-            </ul>
-        );
+    // set total items in slide
+    _setTotalItemsInSlide(cb) {
+        let items = 1;
+        const width = window.innerWidth;
+
+        if (this.props.conf) {
+            const responsive = this.props.conf.responsive || {};
+
+            if (Object.keys(responsive).length) {
+                Object.keys(responsive).forEach((key) => {
+                    if (key < width)
+                        items = responsive[key].items || items;
+                });
+            }
+        }
+        this.setState({ items }, cb);
     }
+
+    _resizeHandler() {
+        this._setTotalItemsInSlide(() => this._getContainerWidth(this.stageElement));
+    }
+
+    _getContainerWidth(el) {
+        const duration = `transform ${this.state.duration}ms ease-out`;
+        const itemWidth = el.getBoundingClientRect().width / this.state.items;
+
+        this.setState({
+            translate3d: 0,
+            currentIndex: 0,
+            itemWidth: itemWidth,
+            style: {
+                transition: duration,
+                OTransition: duration,
+                MozTransition: duration,
+                WebkitTransition: duration
+            }
+        });
+    }
+
+    _getGalleryStageItem(el) {
+        this.stageElement = el;
+        this._setTotalItemsInSlide(() => this._getContainerWidth(el));
+    }
+
+    _allowAnimation() { this.allowAnimation = false; }
+
+    _getCurrentIndex() { return this.state.currentIndex; }
 
     _prevButton() {
         return(
@@ -38,10 +82,37 @@ class Carousel extends React.Component {
         );
     }
 
+    _renderDotsNavigation() {
+        return(
+            <ul className="carousel-dots">
+                <li className="carousel-dots__item __active" />
+                <li className="carousel-dots__item" />
+                <li className="carousel-dots__item" />
+            </ul>
+        );
+    }
+
     render() {
+        const style = Object.assign(
+            {},
+            this.state.style,
+            { transform: `translate3d(${this.state.translate3d}px, 0, 0)` }
+        );
+
         return(
             <div className="carousel">
-                <div className="carousel-stage">Stage</div>
+                <div className="carousel-wrapper">
+                    <GalleryStage stage={ this._getGalleryStageItem } style={ style } >
+                        {
+                            this.state.slides.map((item, i) => (
+                                <li style={{width: `${this.state.itemWidth}px`}}
+                                    className="carousel-stage__item" key={i} >
+                                    { item }
+                                </li>
+                            ))
+                        }
+                    </GalleryStage>
+                </div>
 
                 { this.state.showButtons ? this._prevButton() : null }
                 { this.state.showButtons ? this._nextButton() : null }
@@ -52,3 +123,11 @@ class Carousel extends React.Component {
 }
 
 export default Carousel;
+
+const GalleryStage = ({ stage, style, children }) => {
+    return(
+        <ul className="carousel-stage" ref={ stage } style={ style } >
+            { children }
+        </ul>
+    );
+};
