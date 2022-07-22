924deae1c6ca01012127e3296a3a41c22be52988
maxmarinich <max.marinich@gmail.com>
2022-07-22 09:25:01 +0300

    migrate to v1.18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/app';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const element = document.getElementById('root');
+if (element) {
+	createRoot(element).render(<App />);
+}
